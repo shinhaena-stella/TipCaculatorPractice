e00bbf7290cbff4aaa0d066cbb10cb273082e2e1
CISC Student <cisc@ac.sdmesa.net>
2017-09-28 14:03:22 -0700

    Initial commit

---
+++ app/src/main/java/com/example/cisc/tipcaculatorpractice/MainActivity.java
@@ -0,0 +1,42 @@
+package com.example.cisc.tipcaculatorpractice;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.TextView;
+
+import java.text.NumberFormat;
+
+public class MainActivity extends AppCompatActivity {
+
+    // currency and percent formatter objects
+    private static final NumberFormat currencyFormat =
+            NumberFormat.getCurrencyInstance();
+    private static final NumberFormat percentFormat =
+            NumberFormat.getPercentInstance();
+
+    private double billAmount = 0.0; // bill amount entered by the user
+    private double percent = 0.15; // initial tip percentage
+    private TextView amountTextView; // shows formatted bill amount
+    private TextView percentTextView; // shows tip percentage
+    private TextView tipTextView; // shows calculated tip amount
+    private TextView totalTextView; // shows calculated total bill amount
+
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        amountTextView = (TextView) findViewById(R.id.amountTextView);
+        percentTextView = (TextView) findViewById(R.id.percentTextView);
+        tipTextView = (TextView) findViewById(R.id.tipTextView);
+        totalTextView = (TextView) findViewById(R.id.totalTextView);
+        tipTextView.setText(currencyFormat.format(0));
+        totalTextView.setText(currencyFormat.format(0));
+
+
+
+
+    }
+}
